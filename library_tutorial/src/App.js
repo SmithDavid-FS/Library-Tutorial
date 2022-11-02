4608baa671d5051c79b23f0ba57c4e048c2a2443
David Smith <dasmith3@student.fullsail.edu>
2022-11-02 15:35:47 -0400

    ready for recording

--- library_tutorial/src/App.js
+++ library_tutorial/src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import {orange, blue} from '@mui/material/colors';
+
 
 function App() {
   return (
-    <Grid container rowSpacing={2} columnSpacing={{xs:1, sm: 2, md: 3}}>
+    <Grid container spacing={2} columns={10}>
       <Grid item xs={8}>
-        <Paper>xs=8</Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper>xs=4</Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper>xs=4</Paper>
+        <Box sx={{color: orange[800], bgcolor: blue[200]}}>Testing</Box>
       </Grid>
       <Grid item xs={8}>
-        <Paper>xs=8</Paper>
+        <Box sx={{color: orange[800], bgcolor: blue[200]}}>
+          Testing to test
+          </Box>
+      </Grid>
+      <Grid item xs={8}>
+        <Box sx={{color: orange[800], bgcolor: blue[200]}}>Testing again</Box>
+      </Grid>
+      <Grid item xs={8}>
+        <Box sx={{color: orange[800], bgcolor: blue[200]}}>Testing once more</Box>
       </Grid>
     </Grid>
   );
